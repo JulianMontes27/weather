e36ae9c3fb071acb85bc793187f8649ce65aa56b
Julian Montes <julianmontesPS4@gmail.com>
2023-02-16 00:05:36 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import Hero from './components/Hero';
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <nav className=''>
+            <ul className='flex flex-row list-none justify-center items-center'>
+                <li>HOME</li>
+                <li>CONTACT US</li>
+                <li>ABOUT</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
